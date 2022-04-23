9d39e9eaecd71fc5e41b8cbd3d2cadd13587e70b
zhouhuan <zsean21598@163.com>
2022-04-23 22:52:34 +0800

    变量与可变性

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,20 @@
-use core::num;
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io; // trait 类似于接口
+// 声明常量
+// const MAX_POINTS = 100_000;
 fn main() {
-    println!("猜数游戏!!");
+    // mut 将变量变为可变的
+    let mut x = 5;
+    x = 6;
+    println!("The value of x is {}", x);
 
-    let secret_number = rand::thread_rng().gen_range(1, 11);
-    println!("神秘数字是: {}", secret_number);
+    // shadowing 隐藏
+    // 可以声明同名变量
+    let a = 10;
+    let a = a + 1;
+    println!("The a is {}", a);
 
-    loop {
-        println!("猜测一个数字");
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("无法读取行");
+    // 可以是不同类型
+    let space = "";
+    let space = space.len();
 
-        // rust 允许同名变量,隐藏前面的变量
-        let guess: u32 = match guess.trim().parse() {
-            Ok(number) => number,
-            Err(_) => continue,
-        };
-
-        println!("你猜测的数是:{}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("太小了"), // arm
-            Ordering::Greater => println!("太大了"),
-            Ordering::Equal => {
-                println!("猜中了");
-                break;
-            }
-        }
-    }
+    println!("The space  x is {}", space);
 }
